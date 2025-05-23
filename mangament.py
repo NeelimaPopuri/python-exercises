05f5b5c17b3c0452f9e8064856c1a5d68909b585
NeelimaPopuri <popurineelima5@gmail.com>
2025-05-22 18:15:44 -0700

    intial

---
+++ mangament.py
@@ -0,0 +1,13 @@
+names = []
+ages = []
+emails = []
+
+name = input("Name: ")
+age = input("Age: ")
+email = input("Emails: ")
+
+names.append(name)
+ages.append(age)
+emails.append(email)
+
+print(names, ages, emails)
