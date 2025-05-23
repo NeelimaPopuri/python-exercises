0a59a35c27d42e015e0594ac4bd2d9f65054038e
NeelimaPopuri <popurineelima5@gmail.com>
2025-05-22 18:17:55 -0700

    n

--- mangament.py
+++ mangament.py
@@ -2,12 +2,14 @@ names = []
 ages = []
 emails = []
 
-name = input("Name: ")
-age = input("Age: ")
-email = input("Emails: ")
+for i in range(3):
+    print(i+1, "Input")
+    name = input("Name: ")
+    age = input("Age: ")
+    email = input("Emails: ")
 
-names.append(name)
-ages.append(age)
-emails.append(email)
+    names.append(name)
+    ages.append(age)
+    emails.append(email)
 
 print(names, ages, emails)
